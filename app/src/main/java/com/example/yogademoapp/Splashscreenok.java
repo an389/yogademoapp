0a599d8881b48257be019c7aceb63c404e551592
andum <andu.mihai2000@gmail.com>
2021-06-28 12:52:14 +0300

    yogaapp a

--- app/src/main/java/com/example/yogademoapp/Splashscreenok.java
+++ app/src/main/java/com/example/yogademoapp/Splashscreenok.java
@@ -3,6 +3,7 @@ package com.example.yogademoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.os.Handler;
@@ -10,25 +11,43 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class Splashscreenok extends AppCompatActivity {
 
 
+    Animation up,down;
+    ImageView imageView;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreenok2);
+        setContentView(R.layout.activity_splashscreenok);
+
+        ImageView imageView=findViewById(R.id.appsplash);
+        up=AnimationUtils.loadAnimation(getApplicationContext(),R.id.)
+
+
+        TextView textView=findViewById(R.id.appname);
+        down=AnimationUtils.loadAnimation(getApplicationContext(),R.id.)
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+
+            }
+        },3500);
 
 
     }
 
-    public void imageup(View view){
-        ImageView image= (ImageView)findViewById(R.id.appsplash);
-        Animation animation1=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.up);
-        image.startAnimation(animation1);
 
-    }
 
 
 
